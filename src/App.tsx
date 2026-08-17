e2480a21dd275642573f7e976453cc30eb6cb7c7
Michael Pink <mpink2491@gmail.com>
2026-08-17 16:09:07 -0400

    chore: scaffold Vite+React+TS+Tailwind v4+Vitest project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { GameBoard } from "./components/GameBoard"
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950">
+      <GameBoard />
+    </div>
+  )
+}
+
+export default App
